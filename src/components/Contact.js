70589655bfd01d4615086f518f61c9e0b9bcf548
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-08-31 22:56:13 -0400

    Added delete contact funcionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,6 +12,10 @@ class Contact extends Component {
     });
   };
 
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
@@ -21,9 +25,14 @@ class Contact extends Component {
         <h4>
           {name}
           <i
-            onClick={this.onShowClick}
             className="fas fa-sort-down"
             style={{ cursor: "pointer", marginLeft: "10px" }}
+            onClick={this.onShowClick}
+          />
+          <i
+            className="fas fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+            onClick={this.onDeleteClick}
           />
         </h4>
         {showContactInfo && (
@@ -38,7 +47,8 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -25,13 +25,29 @@ class Contacts extends Component {
     ]
   };
 
+  deleteContact = id => {
+    const { contacts } = this.state;
+
+    const newContacts = contacts.filter(contact => contact.id !== id);
+
+    this.setState({
+      contacts: newContacts
+    });
+  };
+
   render() {
     const { contacts } = this.state;
 
     return (
       <React.Fragment>
         {contacts.map(contact => {
-          return <Contact key={contact.id} contact={contact} />;
+          return (
+            <Contact
+              key={contact.id}
+              contact={contact}
+              deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+            />
+          );
         })}
       </React.Fragment>
     );
