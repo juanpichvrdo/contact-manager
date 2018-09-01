4b5d11a2abbbbfa05bec74b4159838d4f1101244
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-09-01 19:29:39 -0400

    Changed delete functionality to use React Context API

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,55 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-555"
-      },
-      {
-        id: 2,
-        name: "Karen Smith",
-        email: "karen@gmail.com",
-        phone: "333-333-333"
-      },
-      {
-        id: 3,
-        name: "Henry Johnson",
-        email: "henry@gmail.com",
-        phone: "555-555-555"
-      }
-    ]
-  };
-
-  deleteContact = id => {
-    const { contacts } = this.state;
-
-    const newContacts = contacts.filter(contact => contact.id !== id);
-
-    this.setState({
-      contacts: newContacts
-    });
-  };
-
   render() {
-    const { contacts } = this.state;
-
     return (
-      <React.Fragment>
-        {contacts.map(contact => {
+      <Consumer>
+        {value => {
+          const { contacts } = value;
           return (
-            <Contact
-              key={contact.id}
-              contact={contact}
-              deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-            />
+            <React.Fragment>
+              {contacts.map(contact => {
+                return <Contact key={contact.id} contact={contact} />;
+              })}
+            </React.Fragment>
           );
-        })}
-      </React.Fragment>
+        }}
+      </Consumer>
     );
   }
 }
